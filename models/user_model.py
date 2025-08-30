c8ad36363580741fe462b52c32ae67df1c5768c6
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-08-30 20:40:53 +0200

    was added docker compose

--- models/user_model.py
+++ models/user_model.py
@@ -5,6 +5,7 @@ class UserModel(db.Model, SerializableMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
