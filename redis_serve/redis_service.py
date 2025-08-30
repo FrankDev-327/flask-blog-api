6ef7dafa69b3d4b3dd408f12a6ee78fcf5dc0797
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-08-30 22:27:38 +0200

    was added redis service

---
+++ redis_serve/redis_service.py
@@ -0,0 +1,17 @@
+import os
+import redis
+import json
+
+class RedisService:
+    def __init__(self):
+        self.redis = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), decode_responses=True)
+        
+    def setTemporalInfo(self, key, data, ttl=100):
+        json_data = json.dumps(data)
+        self.redis.setex(key, ttl, json_data)
+        
+    def getTemporalInfo(self, key):
+        data = self.redis.get(key)
+        if data is not None:
+            return json.load(data)
+        return None
