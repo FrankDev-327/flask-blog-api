a3a2166eaa15b167af4385d5cd18979cf2cdaf60
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-09-21 21:20:10 +0200

    adding swagger doc package - in process

--- controllers/posts/post_list_controller.py
+++ controllers/posts/post_list_controller.py
@@ -1,10 +1,10 @@
 from base_resource import BaseResource
-from flask_restful import request
+from flask import request
 from middleware.check_token import require_token
 from services.post_service import PostService
 
 class PostListController(BaseResource):
-    method_map = { 'GET': 'getAllPosts' }
+    method_map = {'GET': 'getAllPosts'}
     
     def __init__(self):
         super().__init__()
@@ -12,6 +12,40 @@ class PostListController(BaseResource):
     
     @require_token
     def getAllPosts(self):
-        page = request.args.get('page', 1, type=int)
-        per_page = request.args.get('per_page', 10, type=int)
+        """
+        Get a list of posts
+        ---
+        parameters:
+          - name: page
+            in: query
+            type: integer
+            required: false
+            default: 1
+          - name: per_page
+            in: query
+            type: integer
+            required: false
+            default: 10
+        responses:
+          200:
+            description: List of posts
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                posts:
+                  type: array
+                  items:
+                    type: object
+                    properties:
+                      id:
+                        type: integer
+                      title:
+                        type: string
+                      content:
+                        type: string
+        """
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 10))
         return self.post_service.getAllPosts(page, per_page)
