0d7f20e18a1a1b5025a805eb0715708fa0f7945e
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-09-21 02:57:45 +0200

    still in process

--- services/comment_service.py
+++ services/comment_service.py
@@ -5,7 +5,7 @@ from services.user_service import UserService
 from services.post_service import PostService
 from models.comment_model import CommentModel 
 from middleware.check_token import require_token
-from sqlalchemy import select, insert, join, delete, update
+from sqlalchemy import select, insert, delete, update
 
 helper = Helper()
 
@@ -13,8 +13,6 @@ class CommentService:
     def __init__(self):
         self.logger = LoggerApp()
         self.comment_model = CommentModel
-        self.user_service = UserService()
-        self.post_service = PostService()
 
     @require_token
     def createComment(self, commentBody):
@@ -53,33 +51,56 @@ class CommentService:
 
     @require_token
     def getCommentById(self, comment_id):
-        stmt = (select(CommentModel).where(CommentModel.id == comment_id))
-        comment = db.session.execute(stmt).scalar_one_or_none()
-        if comment:
-            return {
-                "id": comment.id,
-                "content": comment.content,
-                "post_id": comment.post_id,
-                "user_id": comment.user_id,
-                "created_at": helper.formatting_time(comment.created_at, "%Y-%m-%d %H:%M:%S"),
-                "updated_at": helper.formatting_time(comment.updated_at, "%Y-%m-%d %H:%M:%S")
-            }, 200
-        return {'message': 'Comment not found'}, 404
+        try:
+            stmt = (select(CommentModel).where(CommentModel.id == comment_id))
+            comment = db.session.execute(stmt).scalar_one_or_none()
+            if comment:
+                return {
+                    "id": comment.id,
+                    "content": comment.content,
+                    "post_id": comment.post_id,
+                    "user_id": comment.user_id,
+                    "created_at": helper.formatting_time(comment.created_at, "%Y-%m-%d %H:%M:%S"),
+                    "updated_at": helper.formatting_time(comment.updated_at, "%Y-%m-%d %H:%M:%S")
+                }, 200
+            return {'message': 'Comment not found'}, 404
+        except Exception as e:
+            db.session.rollback() 
+            return {'message': f'Error getting details comment: {str(e)}'}, 500
 
     @require_token
     def updateComment(self, comment_id, commentBody):
         if not isinstance(commentBody, dict):
             return {'message': 'Invalid request body format'}, 400
         
-        comment_data, status_code = self.getCommentById(comment_id)
-        if status_code != 200:
-            return {'message': 'Comment not found'}, status_code
-        
-        comment = self.comment_model.query.get(comment_id)
-        comment.content = commentBody['content']
-        db.session.commit()
-
-        return {'message': 'Comment updated', 'comment': comment.to_dict(include_relationships=False)}, 200
+        try:
+            update_stmt = (
+                update(CommentModel)
+                .where(CommentModel.id == comment_id)
+                .values(
+                    content=commentBody['content']
+                ).returning(CommentModel)
+            )
+            
+            result = db.session.execute(update_stmt)
+            row = result.fetchone()
+            db.session.commit()       
+            updated_comment = row[0]
+            
+            return {
+                'message': 'Comment updated', 
+                'comment': {
+                    "id": updated_comment.id,
+                    "content": updated_comment.content,
+                    "post_id": updated_comment.post_id,
+                    "user_id": updated_comment.user_id,
+                    "created_at": helper.formatting_time(updated_comment.created_at, "%Y-%m-%d %H:%M:%S"),
+                    "updated_at": helper.formatting_time(updated_comment.updated_at, "%Y-%m-%d %H:%M:%S")
+                }
+            }, 200
+        except Exception as e:
+            db.session.rollback() 
+            return {'message': f'Error updating comment: {str(e)}'}, 500
 
     @require_token
     def delete(self, comment_id):
