201373da6a916e4325092edc185e91c87ffcab59
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-09-22 09:16:31 +0200

    was added swagger doc for comments

--- controllers/comments/comment_controller.py
+++ controllers/comments/comment_controller.py
@@ -2,51 +2,52 @@ from flask_restful import Resource, request
 from middleware.check_token import require_token
 from services.comment_service import CommentService
 
-class CommentUpdateController(Resource):    
+class CommentController(Resource):    
     def __init__(self):
         super().__init__()
         self.comment_service = CommentService()
-        
+    
     @require_token
-    def put(self, comment_id):
+    def post(self):
         """
-        Update a comment
+        Create a new comment
         ---
         tags:
           - Comment
         parameters:
-          - name: comment_id
-            in: path
-            type: integer
-            required: true
-            description: ID of the comment to update
-          - name: body
-            in: body
+          - in: body
+            name: body
             required: true
             schema:
               type: object
               properties:
                 content:
                   type: string
-                  example: "Updated comment content"
+                  example: "test_content_2000"
+                post_id:
+                  type: integer
+                  example: 1
+                user_id:
+                  type: integer
+                  example: 2
         responses:
-          200:
-            description: Comment updated successfully
+          201:
+            description: Comment successfully created
             schema:
               type: object
               properties:
                 message:
                   type: string
-                  example: "Comment updated"
+                  example: Comment created
                 comment:
                   type: object
                   properties:
                     id:
                       type: integer
-                      example: 6
+                      example: 7
                     content:
                       type: string
-                      example: "baño de semen en el ano de marisol sexo anal en el ano de marisol y despues de dejarle el ano super abierto llenarselo de bastante leche y maria se beba la leche desde el ano de marisol y despues se besen"
+                      example: test_content_2000
                     post_id:
                       type: integer
                       example: 1
@@ -56,19 +57,11 @@ class CommentUpdateController(Resource):
                     created_at:
                       type: string
                       format: date-time
-                      example: "2025-09-21 00:37:08"
+                      example: "2025-09-21 00:51:46"
                     updated_at:
                       type: string
                       format: date-time
-                      example: "2025-09-21 00:49:53"
-          404:
-            description: Comment not found
-            schema:
-              type: object
-              properties:
-                message:
-                  type: string
-                  example: "Comment not found"
+                      example: "2025-09-21 00:51:46"
         """
         comment_body = request.get_json(force=True)
-        return self.comment_service.updateComment(comment_id, comment_body)
+        return self.comment_service.createComment(comment_body)
