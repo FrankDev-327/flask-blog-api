5f30249d5c8a87cfa1f8c3e0fa49e5dd973177aa
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-09-22 10:32:33 +0200

    was added the last swagger doc - user

--- controllers/authentication/auth_user.py
+++ controllers/authentication/auth_user.py
@@ -1,17 +1,53 @@
 from middleware.check_token import generateToken
-from base_resource import BaseResource 
+from flask_restful import Resource, request
 from services.user_service import UserService
 
-class AuthUserController(BaseResource):
-    method_map = { 
-        'POST': 'checkExistinUser'
-    }
-    
+class AuthUserController(Resource):
     def __init__(self):
         super().__init__()
         self.user_service = UserService()
     
-    def checkExistinUser(self, user_body):
+    def post(self):
+        """
+        Authenticate user and return JWT token
+        ---
+        tags:
+          - Auth
+        parameters:
+          - in: body
+            name: body
+            required: true
+            schema:
+              type: object
+              required:
+                - nick_name
+                - password
+              properties:
+                nick_name:
+                  type: string
+                  example: test_user_20
+                password:
+                  type: string
+                  example: 123456789
+        responses:
+          200:
+            description: User successfully authenticated
+            schema:
+              type: object
+              properties:
+                token:
+                  type: string
+                  example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZS..."
+          404:
+            description: User not found or wrong password
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: "user not found or wrong password"
+        """
+        user_body = request.get_json(force=True)
         user = self.user_service.checkExistinUser(user_body)
         token = generateToken(user)
         return {'token': token}
--- routes/users/user_route.py
+++ routes/users/user_route.py
@@ -1,8 +1,7 @@
 from flask_restful import Api
 from controllers.users.user_controller import UserController
+from controllers.users.user_list_all_controller import UserListController
 
 def register_user_routes(api: Api):
-    api.add_resource(UserController, '/user', methods=['GET'], endpoint='user_all')
-    api.add_resource(UserController, '/user', methods=['POST'], endpoint='user_create')
-    #api.add_resource(UserListPostAndCommentController, '/user/<int:user_id>', methods=['GET'], endpoint='user_get')
-    
+    api.add_resource(UserController, '/user', endpoint='user_create')
+    api.add_resource(UserListController, '/user', endpoint='user_all')
