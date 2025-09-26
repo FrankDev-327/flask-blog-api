9c2814d91a1e2d3527564b1983e5b7695f25e3c2
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-09-27 00:44:43 +0200

    was added more features to the test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,31 +1,12 @@
-import os
 import pytest
 import requests
 
-BASE_URL = "http://127.0.0.1:5000/api"
-
 @pytest.mark.order(1)
-def test_health_check_endpoint():
-    response = requests.get(f"{BASE_URL}/health-check")
+def test_health_check_endpoint(api_url):
+    response = requests.get(f"{api_url}/health-check")
     assert response.status_code == 200
     data = response.json()
     assert "message" in data
     assert isinstance(data["message"], str)
     assert data["message"] == "OK"
     
-@pytest.mark.order(2)
-def test_auth_endpoint():
-    response = requests.post(f"{BASE_URL}/auth", json={
-        "password": "123456789",
-        "nick_name": "test_user_20"
-    })
-  
-    assert response.status_code == 200
-    data = response.json()
-    assert "token" in data
-    assert isinstance(data["token"], str)
-    assert len(data["token"]) > 0
-    parts = data["token"].split(".")
-    assert len(parts) == 3
-    token_info_file = open("./token_test.txt", "w")
-    assert token_info_file.write(data["token"]) > 0
--- tests/test_user_exists.py
+++ tests/test_user_exists.py
@@ -1,23 +1,13 @@
-import os
+
 import pytest
 import requests
 
-BASE_URL = "http://127.0.0.1:5000/api"
 
-@pytest.mark.order(4)
-def test_user_exists():
-    user_to_be_created = {
-        "name": "test_user_32",
-        "password": "123456789",
-        "nick_name":"test_nich_name_user_32",
-        "email": "test_user_32@test.com"
-    }
-    
-    with open("./token_test.txt", "r") as f:
-        token_info_file = f.read().strip()
-    assert len(token_info_file) > 0 
-    headers = {"Authorization": f"Bearer {token_info_file}"}
-    response = requests.post(f"{BASE_URL}/user", headers=headers, json=user_to_be_created)
+@pytest.mark.order(3)
+def test_user_exists(user_data, auth_token, api_url):            
+    assert len(auth_token) > 0 
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = requests.post(f"{api_url}/user", headers=headers, json=user_data)
     assert response.status_code == 409
     data = response.json()
     assert "message" in data
