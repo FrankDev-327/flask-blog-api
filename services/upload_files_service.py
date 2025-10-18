c2437feed2a9cd33929bd21d7f26300b23a1ff4e
FrankDev-327 <renteriafrancisco51@gmail.com>
2025-10-18 18:58:22 +0200

    was added cloudinary

--- services/upload_files_service.py
+++ services/upload_files_service.py
@@ -1,38 +1,38 @@
 import os
-from minio import Minio
-from minio.error import S3Error
+import cloudinary
 from logger.logging import LoggerApp
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
 
 
 class UploadFileService:
     def __init__(self):
         self.logger = LoggerApp()
-        self.minio_client = Minio(
-            os.getenv("FIELD_HUB_BUCKET_URL_FLASK"),
-            access_key=os.getenv("FIELD_HUB_BUCKET_ACCESS_KEY"),
-            secret_key=os.getenv("FIELD_HUB_BUCKET_SECRET_KEY"),
-            secure=False,
+        cloudinary.config(
+            cloud_name=os.getenv("CLOUDINARY_NAME"),
+            api_key=os.getenv("CLOUDINARY_API_KEY"),
+            api_secret=os.getenv("CLOUDINARY_SECRET_API_KEY"),
+            secure=True
         )
 
     def upload_image(self, filename, file_data, file_size):
         try:
-            result = self.minio_client.put_object(
-                os.getenv("FIELD_HUB_BUCKET_NAME_FLASK"),
-                filename,
-                file_data,
-                file_size
+            upload_result = upload(file_data)
+            image_url, _ = cloudinary_url(
+                upload_result['public_id'],
+                format="jpg",
+                crop="fill",
+                width=100,
+                height=100
             )
+            
+            return {
+                "message": "Upload successful",
+                "url": image_url,
+                "public_id": upload_result["public_id"]
+            }, 200
 
-            response = {
-                "object_name": filename,
-                "bucket": os.getenv("FIELD_HUB_BUCKET_NAME_FLASK"),
-                "etag": str(result.etag),
-                "version_id": str(result.version_id) if result.version_id else None,
-            }
-
-            print(response)  # for debug
-            return response, 200
-
-        except S3Error as exc:
+        except Exception as exc:
+            # Always convert the exception to string for safe JSON response
             self.logger.logErrorInfo(f"Upload failed: {exc}")
             return {"message": str(exc)}, 500
