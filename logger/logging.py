554183e430209ee3d1a32cb262c12fef9de27228
FrankDev-327 <renteriafrancisco51@gmail.com>
2026-03-19 00:31:59 +0100

    was added run migration

--- logger/logging.py
+++ logger/logging.py
@@ -1,32 +1,27 @@
-import os
 import logging
 
 
 class LoggerApp:
     def __init__(self):
-        self.loggerInfo = logging.getLogger(__name__)
-        self.loggerError = logging.getLogger(__name__)
-        if os.getenv("ENV_APP") == "development":
-            logging.basicConfig(
-                filename="api.log",
-                filemode="w",
-                format="%(asctime)s - %(levelname)s - %(message)s",
-                datefmt="%Y-%m-%d %H:%M:%S",
-                level=logging.INFO,
-            )
+        self.loggerInfo = logging.getLogger('loggerInfo')
+        self.loggerError = logging.getLogger('loggerError')
+        logging.basicConfig(
+            filename="api.log",
+            filemode="w",
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            level=logging.INFO,
+        )
 
     def initLoggerInstance(self):
-        if os.getenv("ENV_APP") == "development":
-            FileOutputHandlerError = logging.FileHandler("error-logs.log")
-            FileOutputHandler = logging.FileHandler("info-logs.log")
+        FileOutputHandlerError = logging.FileHandler("logs/error-logs.log")
+        FileOutputHandler = logging.FileHandler("logs/info-logs.log")
 
-            self.loggerInfo.addHandler(FileOutputHandler)
-            self.loggerError.addHandler(FileOutputHandlerError)
+        self.loggerInfo.addHandler(FileOutputHandler)
+        self.loggerError.addHandler(FileOutputHandlerError)
 
     def logInfoServer(self, infoText):
-        if os.getenv("ENV_APP") == "development":
-            self.loggerInfo.info(infoText)
+        self.loggerInfo.info(infoText)
 
     def logErrorInfo(self, infoToLog):
-        if os.getenv("ENV_APP") == "development":
-            self.loggerError.error(infoToLog)
+        self.loggerError.error(infoToLog)
