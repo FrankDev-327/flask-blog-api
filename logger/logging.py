b7f467a3285019b9f4cfa0c5a0f0da711ce393ce
FrankDev-327 <renteriafrancisco51@gmail.com>
2026-03-19 01:08:12 +0100

    was addes more env variable to the pipeline

--- logger/logging.py
+++ logger/logging.py
@@ -3,19 +3,19 @@ import logging
 
 class LoggerApp:
     def __init__(self):
-        self.loggerInfo = logging.getLogger('loggerInfo')
-        self.loggerError = logging.getLogger('loggerError')
+        self.loggerInfo = logging.getLogger(__name__)
+        self.loggerError = logging.getLogger(__name__)
         logging.basicConfig(
             filename="api.log",
             filemode="w",
             format="%(asctime)s - %(levelname)s - %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
             level=logging.INFO,
-        )
+            )
 
     def initLoggerInstance(self):
-        FileOutputHandlerError = logging.FileHandler("logs/error-logs.log")
-        FileOutputHandler = logging.FileHandler("logs/info-logs.log")
+        FileOutputHandlerError = logging.FileHandler("error-logs.log")
+        FileOutputHandler = logging.FileHandler("info-logs.log")
 
         self.loggerInfo.addHandler(FileOutputHandler)
         self.loggerError.addHandler(FileOutputHandlerError)
